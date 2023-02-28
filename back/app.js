60933334ba860fe3ed699a5a40dd42c31c40b029
David Cardon <da.cardon@outlook.fr>
2023-02-28 15:56:39 +0100

    add mongoose

--- back/app.js
+++ back/app.js
@@ -2,23 +2,33 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-   console.log("Requête reçue !");
-   next();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+   "mongodb+srv://davidflooze:Qd3Ozc2ccPjF6Xo6@cluster0.k1iwwov.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
 });
 
-app.use((req, res, next) => {
-   res.status(201);
-   next();
-});
+app.use(express.json());
 
 app.use((req, res, next) => {
-   res.json({ message: "Votre requête a bien été reçue !" });
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+   );
+   res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+   );
    next();
-});
-
-app.use((req, res, next) => {
-   console.log("Réponse envoyée avec succès !");
 });
 
 module.exports = app;
