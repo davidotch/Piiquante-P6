35016f2d40700480af9c9625346853bb9f54aa1d
David Cardon <da.cardon@outlook.fr>
2023-03-09 17:29:07 +0100

    add helmet&cors

--- back/app.js
+++ back/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
 
 const stuffRoutes = require("./routes/stuff");
 const userRoutes = require("./routes/user");
 
 const path = require("path");
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+const helmet = require("helmet");
+
+const app = express();
 
 mongoose.set("strictQuery", true);
 mongoose
@@ -16,20 +20,11 @@ mongoose
    .then(() => console.log("Connexion à MongoDB réussie !"))
    .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-app.use((req, res, next) => {
-   res.setHeader("Access-Control-Allow-Origin", "*");
-   res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-   );
-   res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-   );
-   next();
-});
+app.use(cors());
 
 app.use(express.json());
+
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 app.use("/api/stuff", stuffRoutes);
 app.use("/api/auth", userRoutes);
