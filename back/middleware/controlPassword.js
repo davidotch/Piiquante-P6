9c384a9b6dc0baf2a885c221883b10dfe616ba16
David Cardon <da.cardon@outlook.fr>
2023-03-14 10:37:57 +0100

    add password-validator

---
+++ back/middleware/controlPassword.js
@@ -0,0 +1,9 @@
+const passWordSchema = require('../models/password');
+
+module.exports = (req, res, next) => {
+    if (!passWordSchema.validate(req.body.passWordSchema)) {
+        res.status(400).json({ message: 'Le mot de passe doit contenir 8 caratères, avec majuscules, minuscules et au moins un chiffres'});
+    } else {
+        next();
+    }
+};
---
+++ back/models/password.js
@@ -0,0 +1,20 @@
+const passWord = require("password-validator");
+
+const passWordSchema = new passWord();
+
+passWordSchema
+   .is()
+   .min(8) // Minimum length 8
+   .is()
+   .max(100) // Maximum length 100
+   .has()
+   .uppercase() // Must have uppercase letters
+   .has()
+   .lowercase() // Must have lowercase letters
+   .has()
+   .digits(2) // Must have at least 2 digits
+   .has()
+   .not()
+   .spaces(); // Should not have spaces
+
+   module.exports = passWordSchema;
