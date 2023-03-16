7001de6ed6ebe3e433f9b9625ab96ea3a613f784
David Cardon <da.cardon@outlook.fr>
2023-03-16 11:11:35 +0100

    add comment and fix CORS

--- back/app.js
+++ back/app.js
@@ -6,15 +6,14 @@ const userRoutes = require("./routes/user");
 
 const path = require("path");
 const mongoose = require("mongoose");
-require("dotenv").config();
 
-const cors = require("cors");
-const helmet = require("helmet");
+const helmet = require("helmet"); //Protection des entêtes http
+require("dotenv").config();
 
 const app = express();
 
-mongoose.set("strictQuery", true);
-mongoose
+mongoose.set("strictQuery", true); //suppression du warning mongoose au demarrage du serveur
+mongoose //connexion à mongoDB
    .connect(process.env.MONGODB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -22,11 +21,24 @@ mongoose
    .then(() => console.log("Connexion à MongoDB réussie !"))
    .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-app.use(cors());
+//Configuration des en-têtes CORS
+app.use((req, res, next) => {
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+   );
+   res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+   );
+   next();
+});
 
 app.use(express.json());
 
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); //Prtection des entêtes http avec helmet
+
 app.use(mongoSanitize()); //Middleware pour protéger contre les injections NoSQL, JavaScript et HTML (Insertion de caractères spéciaux)
 
 app.use("/api/sauces", hotOnesRoutes);
