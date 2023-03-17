268db3e7820e04c4eb5afd6a9271367f61414215
David Cardon <da.cardon@outlook.fr>
2023-03-17 21:57:33 +0100

    fix express-rate-limit

--- back/middleware/rateLimit.js
+++
@@ -1,8 +0,0 @@
-const rateLimit = require("express-rate-limit");
-
-const limiter = rateLimit({
-    windowsMs: 10 * 60 * 1000,
-    max: 10,
-});
-
-module.exports = { limiter: limiter };
--- back/routes/User.js
+++ back/routes/User.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const rateLimit = require("../middleware/rateLimit");
+const rateLimit = require("express-rate-limit");
 
 const userCtrl = require("../controllers/user");
 const controlPassword = require("../middleware/controlPassword");
 
+const limiter = rateLimit({
+   windowsMs: 5 * 60 * 1000,
+   max: 3,
+   messsage: "limite de requête excéder !",
+   headers: true,
+});
+
 router.post("/signup", controlPassword, userCtrl.signup);
-router.post("/login", rateLimit, userCtrl.login);
+router.post("/login", limiter, userCtrl.login);
 
 module.exports = router;
