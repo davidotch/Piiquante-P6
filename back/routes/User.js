0a63ed17a810e31e48af236d05383256bc6a4fb7
David Cardon <da.cardon@outlook.fr>
2023-03-17 17:07:50 +0100

    fix indentation

--- back/routes/User.js
+++ back/routes/User.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const userCtrl = require("../controllers/user");
-const controlPassword = require('../middleware/controlPassword');
+const controlPassword = require("../middleware/controlPassword");
 
 router.post("/signup", controlPassword, userCtrl.signup);
 router.post("/login", userCtrl.login);
