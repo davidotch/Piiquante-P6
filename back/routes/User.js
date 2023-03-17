db09bc8560da3e064f20739a7cdf01b522a177f1
David Cardon <da.cardon@outlook.fr>
2023-03-17 17:10:37 +0100

    import rateLimit to routes

--- back/routes/User.js
+++ back/routes/User.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const rateLimit = require("../middleware/rateLimit");
 
 const userCtrl = require("../controllers/user");
 const controlPassword = require("../middleware/controlPassword");
 
 router.post("/signup", controlPassword, userCtrl.signup);
-router.post("/login", userCtrl.login);
+router.post("/login", rateLimit, userCtrl.login);
 
 module.exports = router;
